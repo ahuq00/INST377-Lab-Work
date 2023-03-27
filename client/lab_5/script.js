d77ae7432497d855be205ea6e10bbc117710ce79
amanuhuq <73295694+amanuhuq@users.noreply.github.com>
2023-03-27 07:19:42 -0400

    pushing again because repository is not showing the changes but local host is

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -5,17 +5,24 @@
 
 /* A quick filter that will return something based on a matching input */
 function filterList(list, query) {
+  return list.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+
+  })
   /*
     Using the .filter array method, 
     return a list that is filtered by comparing the item name in lower case
     to the query in lower case
-
     Ask the TAs if you need help with this
   */
 }
 
 async function mainEvent() { // the async keyword means we can make API requests
-  const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+  const mainForm = document.querySelector('.main_form');
+  const filterButton = document.querySelector('.filter_button');
+  // This class name needs to be set on your form before you can listen for an event on it
   // Add a querySelector that targets your filter button here
 
   let currentList = []; // this is "scoped" to the main event function
@@ -54,15 +61,27 @@ async function mainEvent() { // the async keyword means we can make API requests
     console.table(currentList); 
   });
 
+  filterButton.addEventListener('click',(event) =>{
+  console.log('clicked filterButton');
+  
+  const formData = new FormData(mainForm);
+  const formProps = Object.fromEntries(formData);
+  
+  console.log(formProps);
+  const newList = filterList(currentList,formProps.resto);
+
+  console.log(newList);
+
+  })
+
+
 
   /*
     Now that you HAVE a list loaded, write an event listener set to your filter button
     it should use the 'new FormData(target-form)' method to read the contents of your main form
     and the Object.fromEntries() method to convert that data to an object we can work with
-
     When you have the contents of the form, use the placeholder at line 7
     to write a list filter
-
     Fire it here and filter for the word "pizza"
     you should get approximately 46 results
   */
